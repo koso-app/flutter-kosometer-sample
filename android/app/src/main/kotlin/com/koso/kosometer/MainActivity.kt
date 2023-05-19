901b5f6e10e5e6bf52c52e45c15906ffc585d1d0
Shawn Lu <shawn@whiles.app>
2023-05-19 13:01:44 +0800

    Added notification listener permission check

--- android/app/src/main/kotlin/com/koso/kosometer/MainActivity.kt
+++ android/app/src/main/kotlin/com/koso/kosometer/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.koso.kosometer
 
+import android.app.AlertDialog
+import android.content.ComponentName
+import android.content.DialogInterface
+import android.content.Intent
+import android.provider.Settings
+import android.text.TextUtils
 import com.koso.kosometer.channel.Talkie
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    private val ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners"
+    private val ACTION_NOTIFICATION_LISTENER_SETTINGS =
+        "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"
 
     override fun onStart() {
         super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(!isNotificationServiceEnabled()){
+            buildNotificationServiceAlertDialog().show()
+        }
     }
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -17,6 +33,40 @@ class MainActivity: FlutterActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Talkie._instance?.onDestory()
+    }
+
+    private fun buildNotificationServiceAlertDialog(): AlertDialog {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Notification Listener Service")
+        alertDialogBuilder.setMessage("For the the app. to work you need to enable the Notification Listener Service. Enable it now?")
+        alertDialogBuilder.setPositiveButton("YES",
+            DialogInterface.OnClickListener { dialog, id -> startActivity(Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS)) })
+        alertDialogBuilder.setNegativeButton("NO",
+            DialogInterface.OnClickListener { dialog, id ->
+                // If you choose to not enable the notification listener
+                // the app. will not work as expected
+            })
+        return alertDialogBuilder.create()
+    }
+
+    private fun isNotificationServiceEnabled(): Boolean {
+        val pkgName = packageName
+        val flat = Settings.Secure.getString(
+            contentResolver,
+            ENABLED_NOTIFICATION_LISTENERS
+        )
+        if (!TextUtils.isEmpty(flat)) {
+            val names = flat.split(":").toTypedArray()
+            for (i in names.indices) {
+                val cn = ComponentName.unflattenFromString(names[i])
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.packageName)) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
     }
 
 }
